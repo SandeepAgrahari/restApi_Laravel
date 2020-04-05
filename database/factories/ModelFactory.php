f833d372be41c4030d7abc5e3ffb74d2d9101eba
Sandeep Kumar <sagrahari399@gmail.com>
2020-04-05 10:38:29 +0530

    Factory and Seeder => Doneand User Controller operation => Inprocess

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Category;
+use App\Product;
+use App\Seller;
+use App\Transaction;
 use App\User;
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +26,7 @@ $factory->define(User::class, function (Faker\Generator $faker) {
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
         'varified' => $varified = $faker->randomElement([User::VARIFIED_USER, User::UNVARIFIED_USER]),
-        'varification_token' => $varified == User::VARIFIED_USER ? null :  User::generateVarificationCode,
+        'varification_token' => $varified == User::VARIFIED_USER ? null :  User::generateVarificationCode(),
         'admin' => $varified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
     ];
 });
@@ -32,6 +35,30 @@ $factory->define(Category::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->word,
-        'email' => $faker->paragraph(1),
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image'=> $faker->randomElement(['babybottle.png', 'cap.png', 'dettole.jpg']),
+        'seller_id' => User::all()->random()->id,
+        //'seller_id' => User::inRandomOrder()->first()->id
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker\Generator $faker) {
+
+    $seller = Seller::has('products')->get()->random();
+    $buyer = User::all()->except($seller->id)->random();
+    return [
+        'quantity' => $faker->numberBetween(1,3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
